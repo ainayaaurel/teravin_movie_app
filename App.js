17b6e1e39335268b84660a176205a5482d3ee1e5
ainayaaurel <ainayaass@gmail.com>
2020-06-09 07:25:48 +0700

    add netinfo

--- App.js
+++ App.js
@@ -1,17 +1,66 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
+// import React, {Component} from 'react';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import {store, persistor} from './src/redux/store';
 import MainHome from './src/screens/StackScreen';
+import NetInfo from '@react-native-community/netinfo';
+import {SafeAreaView, Text, View} from 'react-native';
+import {Image} from 'react-native-elements';
 
-export default class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <MainHome />
-        </PersistGate>
-      </Provider>
-    );
+const App = () => {
+  const [isInternetReachable, setIsInternetReachable] = useState(false);
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener((state) => {
+      setIsInternetReachable(state.isInternetReachable);
+    });
+    return () => {
+      unsubscribe;
+    };
+  }, []);
+  if (isInternetReachable) {
+    return null;
   }
-}
+  return (
+    <View
+      style={{
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1,
+        backgroundColor: 'white',
+      }}>
+      <View>
+        <Image
+          style={{
+            width: 400,
+            height: 250,
+          }}
+          source={require('./src/assets/image/nointernet.png')}
+          resizeMode="contain"
+        />
+      </View>
+      <View>
+        <Text style={{textAlign: 'center', fontSize: 28, fontWeight: 'bold'}}>
+          Ooops!
+        </Text>
+        <Text style={{textAlign: 'center', fontSize: 18}}>
+          No Internet Connection Found
+        </Text>
+        <Text>Check your connection</Text>
+      </View>
+    </View>
+  );
+};
+
+export default App;
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <Provider store={store}>
+//         <PersistGate loading={null} persistor={persistor}>
+//           <MainHome />
+//         </PersistGate>
+//       </Provider>
+//     );
+//   }
+// }
